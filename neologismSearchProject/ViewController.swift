fe316f708626cd5f18695ba73ee7bccbb9fe2f45
sixteenis <psm0103907@naver.com>
2024-05-18 20:41:09 +0900

    (뷰) 검색창 뷰 개발

--- neologismSearchProject/ViewController.swift
+++ neologismSearchProject/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var mainTextField: UITextField!
+    @IBOutlet var mainTextFiledButton: UIButton!
+    
+    @IBOutlet var subSearchButton1: UIButton!
+    @IBOutlet var subSearchButton2: UIButton!
+    @IBOutlet var subSearchButton3: UIButton!
+    
+    @IBOutlet var mainbackgroundImage: UIImageView!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var subbackgroundImage: UIImageView!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setView()
+    }
+    func setView() {
+        mainTextField.layer.borderWidth = 3
+        mainTextField.layer.borderColor = UIColor.black.cgColor
+        mainTextField.frame.size.height = 45
+        
+        mainTextFiledButton.setImage(.init(systemName: "magnifyingglass"), for: .normal)
+        mainTextFiledButton.tintColor = .white
+        mainTextFiledButton.backgroundColor = .black
+        mainTextFiledButton.setTitle("", for: .normal)
     }
 
 
